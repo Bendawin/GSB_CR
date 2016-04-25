945363c7825118cb2fec4443b98451a77f614ad6
MaximeHomm <maximevolant@free.fr>
2016-04-25 11:30:20 +0200

    Modification de la page v_visAccueil

--- application/views/v_visAccueil.php
+++ application/views/v_visAccueil.php
@@ -1,21 +1,21 @@
 <div id="contenu">
-	<h2>Gestion des frais des visiteurs</h2>
-	<p>Bienvenue dans votre application de gestion des frais de déplacements. </p>
+	<h2>Gestion des frais des comptes-rendus</h2>
+	<p>Bienvenue dans votre application de gestion des comptes-rendus. </p>
 	<p>
-		Afin de couvrir les frais que vous avez engagés lors de vos déplacements professionnels, vous devez compléter des fiches de frais mensuelles.
+		Afin de fournir vos rapports de visite, des informations sur votre médecin traitant ou encore les états de frais pour lesquels vous percevez des remboursements, vous devez compléter des fiches de comptes-rendus. Voici des informations utiles les concernant:
 		<ul>
-			<li>Une fiche de frais court du 1er au dernier jour du mois;
-			<li>Les fiches de frais sont créées automatiquement par l'application au fil de votre utilisation;
-			<li>Vous complétez les fiches de frais à votre rythme;
-			<li>Lorsqu'une fiche est totalement renseignée, vous devrez la "signer" afin qu'elle puisse être traitée par le service comptable;
+			<li>Un compte-rendu court du 1er au dernier jour du mois;
+			<li>Les comptes-rendus sont créés automatiquement par l'application au fil de votre utilisation;
+			<li>Vous complétez les comptes-rendus à votre rythme;
 		</ul>
 	</p>
 
 	<p>
-		Au moyen du bandeau gauche, vous avez accès aux fonctionalités 
+		Au moyen du bandeau gauche, vous avez accès aux fonctionnalités 
 		du profil visiteur : 
 		<ul>
-			<li>Gérer vos fiches de frais (modifier, signer, imprimer)</li>
+			<li>Ajouter un compte-rendu</li>
+			<li>Voir les comptes-rendus</li>
 			<li>Se déconnecter</li>
 		</ul>
 	</p>
