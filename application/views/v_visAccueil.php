c9bba0928c415bb847be84d5c2fb85ed7d49e441
MaximeHomm <maximevolant@free.fr>
2016-04-28 15:03:09 +0200

    Modification terre-à-terre

--- application/views/v_visAccueil.php
+++ application/views/v_visAccueil.php
@@ -1,18 +1,12 @@
 <div id="contenu">
 	<h2>Gestion des comptes-rendus</h2>
-	<p>Bienvenue dans votre application de gestion des comptes-rendus. </p>
+	<h4>Bienvenue dans votre application de gestion des comptes-rendus. </h4>
 	<p>
-		Afin de fournir vos rapports de visite, des informations sur votre médecin traitant ou encore les états de frais pour lesquels vous percevez des remboursements, vous devez compléter des fiches de comptes-rendus. Voici des informations utiles les concernant:
-		<ul>
-			<li>Un compte-rendu court du 1er au dernier jour du mois;
-			<li>Les comptes-rendus sont créés automatiquement par l'application au fil de votre utilisation;
-			<li>Vous complétez les comptes-rendus à votre rythme;
-		</ul>
+		Afin de fournir vos rapports de visite, vous devez compléter des comptes-rendus.
 	</p>
 
 	<p>
-		Au moyen du bandeau gauche, vous avez accès aux fonctionnalités 
-		du profil visiteur : 
+		Au moyen du bandeau gauche, vous avez accès aux fonctionnalités du profil visiteur : 
 		<ul>
 			<li>Ajouter un compte-rendu</li>
 			<li>Voir les comptes-rendus</li>
